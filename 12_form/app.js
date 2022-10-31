6d874872178ac17eb627899287f126d45108e647
kimyena1123 <kyq104@gmail.com>
2022-10-31 10:31:00 +0900

    study : app.js use와 json 코드와  설명주석 추가

--- 12_form/app.js
+++ 12_form/app.js
@@ -2,12 +2,23 @@ const express = require('express');
 const app = express();
 const PORT = 8000;
 
-app.set('view engine', 'ejs'); //view engine 등록
-app.use('/views', express.static(__dirname + 'views'));// views 파일 설정
 
-app.get('/', function(req, res){
+//views 설정
+app.set('view engine', 'ejs'); //view engine 등록
+//Middleware(미들웨어) 설정
+// : 요청(req) 과 응답(res)의 중간에서 작업
+app.use('/views', express.static(__dirname + 'views'));// views 파일 설정
+app.use(express.urlencoded({extended : true}));
+app.use(express.json()); // json 형태로 데이터를 전달받음
+
+
+
+// Routing - 경로 설정
+//-req(request) : 요청 (cliend - > sever)
+//-res(response) : 응답 (server -> client)
+app.get('/', function(req, res){ // GET / (http://locahost:PORT)
     //res.send('root page);
-    res.render('index'); ///views/index.ejs 파일을 찾아서 응담
+    res.render('index'); ///views/index.ejs 파일을 찾아서 clien에게 "응답"함.
 });
 
 app.listen(PORT, function(){
